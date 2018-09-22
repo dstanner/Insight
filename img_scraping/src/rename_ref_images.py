1a76a7ebe76acb9befbe37ce3a764aed31a5c97d
Darren Tanner <34904658+dstanner@users.noreply.github.com>
2018-09-22 16:23:00 -0700

    Fixed file paths

--- img_scraping/src/rename_ref_images.py
+++ img_scraping/src/rename_ref_images.py
@@ -5,24 +5,27 @@ totalwine = []
 liquormart = []
 winetoship = []
 
-for path, directory, files in os.walk('../raw_ref_img/totalwine/'):
+for path, directory, files in os.walk('./img_scraping/raw_ref_img/totalwine/'):
     for name in files:
         totalwine.append(name)
-        
-for path, directory, files in os.walk('../raw_ref_img/liquormart/'):
+
+for path, directory, files in os.walk('./img_scraping/raw_ref_img/liquormart/'):
     for name in files:
         liquormart.append(name)
-        
-for path, directory, files in os.walk('../raw_ref_img/winetoship/'):
+
+for path, directory, files in os.walk('./img_scraping/raw_ref_img/winetoship/'):
     for name in files:
         winetoship.append(name)
 
 
 for file in totalwine:
-    shutil.copy('../raw_ref_img/totalwine/' + file, '../raw_ref_img/full/totalwine_'+file)
+    shutil.copy('./img_scraping/raw_ref_img/totalwine/' + file,
+                './img_scraping/raw_ref_img/full/totalwine_' + file)
 
 for file in liquormart:
-    shutil.copy('../raw_ref_img/liquormart/' + file, '../raw_ref_img/full/liquormart_'+file)
-    
+    shutil.copy('./img_scraping/raw_ref_img/liquormart/' + file,
+                './img_scraping/raw_ref_img/full/liquormart_' + file)
+
 for file in winetoship:
-    shutil.copy('../raw_ref_img/winetoship/' + file, '../raw_ref_img/full/winetoship_'+file)
+    shutil.copy('./img_scraping/raw_ref_img/winetoship/' + file,
+                './img_scraping/raw_ref_img/full/winetoship_' + file)
