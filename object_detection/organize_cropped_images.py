9945440ad989310876047643db46c4464f05f3a2
Darren Tanner <34904658+dstanner@users.noreply.github.com>
2018-09-23 10:27:12 -0700

    Add

---
+++ object_detection/organize_cropped_images.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import os
+import re
+from shutil import copyfile
+
+# Load metadata
+info = pd.read_csv('./img_scraping/item_info/all_item_info_categorized.csv')
+
+# Get list of cropped images
+cropped_files = os.listdir('./object_detection/ref_img_cropping/cropped_images/')
+cropped_files
+
+# Extract source_name from cropped images
+cropped_names = [re.sub('crop_[0-9]_','', x) for x in cropped_files]
+cropped_names = [re.sub('.png', '', x) for x in cropped_names]
+cropped_names = [x for x in cropped_names if '.DS' not in x]
+cropped_files
+
+
+# Get source_name from metadata df
+sources = info.source.tolist()
+names = info.Name.tolist()
+names = [x.replace(" ", "_") for x in names]
+
+source_names = []
+for i in range(len(names)):
+    source_names.append(sources[i] + '_' + names[i])
+
+# Get categories from df
+cats = info.Cat.tolist()
+
+# Get categories for each name
+pic_categories = {}
+for name in cropped_names:
+    for ind, string in enumerate(source_names):
+        if name in string:
+            pic_categories[name] = cats[ind]
+
+included_categories = list(set(pic_categories.values()))
+
+# Make directories for each category
+path = './object_detection/cat_organized_ref_img/'
+
+for cat in included_categories:
+    os.makedirs(path + cat)
+
+# Prepare to copy the files
+source_dir = './object_detection/ref_img_cropping/cropped_images/'
+dest_dir = './object_detection/cat_organized_ref_img/'
+
+# copy files to category directories, and save a dictionary
+pic_lookup_dictionary = {}
+for name, cat in pic_categories.items():
+    for file in cropped_files:
+        if name in file:
+            copyfile(source_dir + file, dest_dir + cat + '/' + file)
+            pic_lookup_dictionary[file] = cat
+
+pic_lookup_dictionary
